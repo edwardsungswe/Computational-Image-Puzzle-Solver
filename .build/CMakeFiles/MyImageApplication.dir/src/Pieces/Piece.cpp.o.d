a24d84e5258450b6463b62cb01c49ca3ee774dc6
edwardsung4217 <edwardsung4217@gmail.com>
2025-12-08 01:27:41 -0800

    Upload .build file for testing purpose

---
+++ .build/CMakeFiles/MyImageApplication.dir/src/Pieces/Piece.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/MyImageApplication.dir/src/Pieces/Piece.cpp.o: \
+  /Users/edwardsung/Desktop/repo/CSCI_576_Proj_1/src/Pieces/Piece.cpp
---
+++ .build/CMakeFiles/MyImageApplication.dir/src/config.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/MyImageApplication.dir/src/config.cpp.o: \
+  /Users/edwardsung/Desktop/repo/CSCI_576_Proj_1/src/config.cpp \
+  /Users/edwardsung/Desktop/repo/CSCI_576_Proj_1/src/config.h
