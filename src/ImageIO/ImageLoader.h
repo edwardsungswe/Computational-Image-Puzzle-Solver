86be48cae5e65f53b4de37ec7cd2e96609a9efff
Edward Sung <edwardsung4217@gmail.com>
2025-12-15 16:54:54 -0800

    clear

--- src/ImageIO/ImageLoader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include <string>
-
-class ImageLoader {
-public:
-    static cv::Mat loadPNG(const std::string& path);
-    static unsigned char* loadRawRGB(const std::string& path, int width, int height);
-};
--- src/Pieces/PieceExtractor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include <vector>
-
-class PieceExtractor {
-public:
-    static std::vector<cv::Mat> extractPieces(const cv::Mat& rgbImage);
-    static cv::Mat drawContoursOnImage(const cv::Mat& rgbImage);
-};
